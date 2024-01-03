32763d6b51c878c6383e2dd50b5fd736810fa57e
Olamide Alokan <alokanolamide@gmail.com>
2024-01-03 10:40:01 +0100

    [FIXED] 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,13 @@
 export default function taskBlock(trueOrFalse) {
+  // eslint-disable-next-line no-var
   var task = false;
+  // eslint-disable-next-line no-var
   var task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true;
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
